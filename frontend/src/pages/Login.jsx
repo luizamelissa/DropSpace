034dd7baa2d2555887c5569ef2e39a799fe43b92
luizamelissa <melilux08@gmail.com>
2025-06-26 22:33:34 -0300

    Rotas e páginas base

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,4 @@
+function Login() {
+return <h1>Página de Login</h1>;
+}
+export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,4 @@
+function Register() {
+return <h1>Página de Cadastro</h1>;
+}
+export default Register;
